193ba874c2702ada78e74dcd4793f41363984ba8
Katie <katiepino@gmail.com>
2016-09-28 19:46:49 -0700

    Removed stemming from text
    Stemming did not seem to work as intended

--- text/firstusprezdebate2016/debateWordClouds.r
+++ text/firstusprezdebate2016/debateWordClouds.r
@@ -14,24 +14,21 @@ holt_text <- holt$TEXT
 clinton_text <- clinton$TEXT
 trump_text <- trump$TEXT
 
-#for all speakers: create a corpus, convert to plain text, remove punctuation, remove stopwords, perform stemming
+#for all speakers: create a corpus, convert to plain text, remove punctuation, remove stopwords
 holtCorpus <- Corpus(VectorSource(holt_text))
 holtCorpus <- tm_map(holtCorpus, PlainTextDocument)
 holtCorpus <- tm_map(holtCorpus, removePunctuation)
 holtCorpus <- tm_map(holtCorpus, removeWords, stopwords('english'))
-holtCorpus <- tm_map(holtCorpus, stemDocument)
 
 clintonCorpus <- Corpus(VectorSource(clinton_text))
 clintonCorpus <- tm_map(clintonCorpus, PlainTextDocument)
 clintonCorpus <- tm_map(clintonCorpus, removePunctuation)
 clintonCorpus <- tm_map(clintonCorpus, removeWords, stopwords('english'))
-clintonCorpus <- tm_map(clintonCorpus, stemDocument)
 
 trumpCorpus <- Corpus(VectorSource(trump_text))
 trumpCorpus <- tm_map(trumpCorpus, PlainTextDocument)
 trumpCorpus <- tm_map(trumpCorpus, removePunctuation)
 trumpCorpus <- tm_map(trumpCorpus, removeWords, stopwords('english'))
-trumpCorpus <- tm_map(trumpCorpus, stemDocument)
 
 #plot wordcloud for each speaker
 wordcloud(holtCorpus, max.words = 100, random.order = FALSE)
